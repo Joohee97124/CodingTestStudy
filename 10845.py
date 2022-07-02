cc0c2471d614181426d98b81b858b5377a94cd74
[Joohee97124] <asdqorrn1@naver.com>
2022-07-02 22:34:40 +0900

    20220702

---
+++ 10845.py
@@ -0,0 +1,35 @@
+#백준 10845 큐
+
+
+import sys
+n = int(sys.stdin.readline())
+
+qu=[]
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push':
+        qu.append(command[1])
+    elif command[0] == 'pop':
+        if len(qu)==0:
+            print(-1)
+        else:
+            print(qu[0])
+            qu.pop(0)
+    elif command[0] == 'size':
+        print(len(qu))
+    elif command[0] == 'empty':
+        if len(qu)==0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if len(qu) == 0: 
+          print(-1)
+        else: 
+          print(qu[0])
+    elif command[0] == 'back':
+        if len(qu) == 0: 
+          print(-1)
+        else: 
+          print(qu[-1])
